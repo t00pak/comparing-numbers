072d05bfe4bda9b0255964f295b6ee32ceee56e9
t00pak <130712912+t00pak@users.noreply.github.com>
2025-07-24 23:41:08 -0400

    Python script

---
+++ Comparing Numbers.py
@@ -0,0 +1,10 @@
+n1 = int(input("Please enter a number. "))
+n2 = int(input(" Please enter a second number. "))
+
+if n1 < n2:
+    print(n1, " is the smaller number.")
+else:
+    print(n1, "is larger than", n2,".")
+
+if n1 == n2:
+    print("The numbers are equal.")
